8e0ea7d66ddc3cdf0050709832b994218a4c9800
gz8844 <gz8844@gmail.com>
2025-05-09 10:58:12 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -12,10 +12,10 @@ const PASSWORD_CONFIG = {
 
 // 网站信息配置
 const SITE_CONFIG = {
-    name: 'LibreTV',
-    url: 'https://libretv.is-an.org',
-    description: '免费在线视频搜索与观看平台',
-    logo: 'https://images.icon-icons.com/38/PNG/512/retrotv_5520.png',
+    name: 'GQCtv',
+    url: 'https://vod.jx.al',
+    description: 'GQCtv免费在线视频搜索与观看平台',
+    logo: 'https://p0.meituan.net/csc/05e9a27d1d83478cd5475ac439d505f27627.png',
     version: '1.0.3'
 };
 
